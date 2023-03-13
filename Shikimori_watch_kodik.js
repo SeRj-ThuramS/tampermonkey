5cd9c3e717500d6ea1df226abe23e1477b0f3a17
Makarov Dreyar <80548743+SeRj-ThuramS@users.noreply.github.com>
2023-03-13 16:01:31 +0300

    ывыаыва
    ыаыаываывавы

--- Shikimori_watch_kodik.js
+++ Shikimori_watch_kodik.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Shikimori watch
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  try to take over the world!
 // @author       You
 // @match        https://shikimori.one/*
